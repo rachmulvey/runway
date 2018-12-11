3a88a1a160d398181935b20ef24809a69ccd812c
Tyson Wolker <tysonwolker@gmail.com>
2018-12-11 14:19:12 +1100

    feat(automation test): added button automation tests and fixed ci

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,1 +1,15 @@
-export { default } from './Button';
+import React from "react";
+
+const Button = ({ className, label, ...buttonProps }) => (
+  <div className={className}>
+    <button {...buttonProps}>
+      {label}
+    </button>
+  </div>
+);
+
+Button.defaultProps = {
+  label: "button"
+};
+
+export default Button;
